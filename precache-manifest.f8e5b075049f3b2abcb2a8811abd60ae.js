a35b058cbe07df8aeb22f2cb0c64029e25321f36
Joaquin Cicetti <jcicetti@menlo-technologies.com>
2019-10-17 21:23:02 -0300

    Updates

---
+++ precache-manifest.f8e5b075049f3b2abcb2a8811abd60ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f972efa104066628b8696012f2535b2d",
+    "url": "/JoaquinCicetti/cocktail-finder/index.html"
+  },
+  {
+    "revision": "ed8e0676fde0cd1cd726",
+    "url": "/JoaquinCicetti/cocktail-finder/static/css/main.eb4d9fc9.chunk.css"
+  },
+  {
+    "revision": "5ccbbde81b76f75239ad",
+    "url": "/JoaquinCicetti/cocktail-finder/static/js/2.04529b86.chunk.js"
+  },
+  {
+    "revision": "ed8e0676fde0cd1cd726",
+    "url": "/JoaquinCicetti/cocktail-finder/static/js/main.909e971b.chunk.js"
+  },
+  {
+    "revision": "738b6c982279a20dcb78",
+    "url": "/JoaquinCicetti/cocktail-finder/static/js/runtime-main.ad5e93dc.js"
+  }
+]);
