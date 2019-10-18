6033f80356d5c97ebfc720d557029d1b990c06fe
Joaquin Cicetti <jcicetti@menlo-technologies.com>
2019-10-17 21:10:59 -0300

    added discard old fetch resolved

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -2,6 +2,8 @@ import { Action } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import * as ListReducer from '../interfaces/ListReducer';
 import { Cocktail, APIResponse } from '../interfaces/Cocktail';
+// avoid unsorted resolve of API request
+let TIMESTAMP = new Date().getTime();
 
 // clear cocktail list
 export const clearCocktails = () => ({
@@ -23,15 +25,20 @@ export const receiveCocktails = (cocktails: Array<Cocktail>) => ({
 // send search request
 export const search = (
     value: string,
-): ThunkAction<void, ListReducer.State, null, Action<string>> => async dispatch => {
+): ThunkAction<void, ListReducer.State, null, Action<string>> => dispatch => {
     // set status
     dispatch(requestCocktails(value));
-
+    const sent = new Date().getTime();
     // return a new promise
     return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
         .then((response: Response) => {
             return response.json();
         })
-        .then((json: APIResponse) => dispatch(receiveCocktails(json.drinks)))
+        .then((json: APIResponse) => {
+            if (TIMESTAMP < sent) {
+                dispatch(receiveCocktails(json.drinks));
+                TIMESTAMP = sent;
+            } else console.log('se rompio');
+        })
         .catch(error => console.log('An error occurred.', error));
 };
